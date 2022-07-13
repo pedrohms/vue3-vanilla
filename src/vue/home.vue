336a2f974d0d80fca9a6a1eb5eff0c027916aef1
Pedro Souza <pedrohms412@gmail.com>
2022-07-13 16:17:20 -0300

    Initial commit

---
+++ src/vue/home.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue"
+let msg = "Hi mom !"
+let count = ref(0)
+const handleclick = () => {
+  console.log("ok")
+  console.log(count.value)  
+    count.value = count.value + 1
+  }
+</script>
+<template>
+  <div>
+    <h1>{{ msg }} - {{ count }}</h1>
+    <button @click="handleclick">Click me</button>
+  </div>
+</template>
+<style scoped>
+  .example {
+    color: v-bind("color");
+  }
+</style>
